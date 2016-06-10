fb8a0b3bf62a19321a8b8a49f52e10f9e613c6c5
money <manmohit.singh@gmail.com>
2016-06-09 21:48:08 -0700

    fix redirect after registeration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,11 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    case
-    when resource.first_name.blank?
-      redirect_to new_profile_path
-    else
-      profile_path(resource.profile)
-    end
+    new_profile_path
   end
 end
